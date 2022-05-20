a9b822882a0a870105a566488ce48436d13f600d
alh <alh@CCHJUL2018>
2022-05-20 12:28:38 +0200

    latest commit

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Outlet } from "react-router-dom";
 import { AuthProvider } from "./context/AuthProvider";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
